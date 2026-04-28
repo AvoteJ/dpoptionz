25e0b2e9ffa305b4574f9ce58fbf46f810a611ca
AvoteJ <kis03143@gmail.com>
2026-04-28 22:18:55 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Google Sheet : DPOPz Option DATABASE SHEET
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx0RCj-ZFS2_xoYLoM_mW3aEH-gt4yvq0GCXnaOZKx3fQtzXsZKhdaMjSJ0xRU5tw7t/exec'; // Google Sheet : DPOPz 옵션 저장 테이블
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyZ5_moOTi1uojZ0YG70icTNleTigbO9qXcMAnm9DYup97dJE7L16BgN1nG1aFSYRHs/exec'; // Google Sheet : DPOPz 옵션 저장 테이블
 const APPS_SCRIPT_TABLE = 'My table sheet' // Google Sheet : DPOPz 곡명 리스트 저장 테이블
