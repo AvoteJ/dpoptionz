12278cce31294fa18d548e89d6dc9b1c2525cd77
AvoteJ <kis03143@gmail.com>
2026-05-03 14:53:12 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ※ Google Apps Script 배포 후 아래 URL을 교체하세요
 // 이 파일 하나만 수정하면 모든 페이지에 반영됩니다.
-const APPS_SCRIPT_URL = https://script.google.com/macros/s/AKfycbw2zexeGnyEIK9gw6WXwPsUXlqBmYOeAtEdtbICHNjydNq_qO_GivqN0mv-OjrurS1n/exec;
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw2zexeGnyEIK9gw6WXwPsUXlqBmYOeAtEdtbICHNjydNq_qO_GivqN0mv-OjrurS1n/exec';
