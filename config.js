cd78da5d79b05729d772341615d1da09dd23a6f7
AvoteJ <kis03143@gmail.com>
2026-05-03 17:58:45 +0900

    Update config.js

--- config.js
+++ config.js
@@ -15,9 +15,9 @@ const APP_VERSION = '26.05.02[BETA]';
 // BANNER_LINES : 배너 문장 배열 (빈 배열 [] 이면 배너 전체 숨김)
 const BANNER_LABEL = '<CLOSED TEST>';
 const BANNER_LINES = [
-    '모든 페이지가 정상적으로 작동하지 않을 우려가 있습니다. 만약 오류가 발생할 경우, <Report> 메뉴를 통해 제보해주시면 감사드리겠습니다.',
-    'まだ未完成のため、エラーが発生する恐れがあります。もしエラーが発生した場合＜Report＞メニューでご報告のご協力お願いします。',
-    'Currently, errors may occur in all functions. If an error occurs, please report it through the <Report> menu.',
+    '모든 페이지가 정상적으로 작동하지 않을 우려가 있습니다. 만약 오류가 발생할 경우, 「Report」 메뉴를 통해 제보해주시면 감사드리겠습니다.',
+    'まだ未完成のため、エラーが発生する恐れがあります。もしエラーが発生した場合「Report」メニューでご報告のご協力お願いします。',
+    'Currently, errors may occur in all functions. If an error occurs, please report it through the 「Report」 menu.',
 ];
 
 // ── 푸터 텍스트 ──────────────────────────────
